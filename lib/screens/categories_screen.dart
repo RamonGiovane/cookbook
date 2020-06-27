9d4876cc8951df46f195eb356a158137aade8bc4
RamonGiovane <ramon.giovane@gmail.com>
2020-06-27 17:02:35 -0300

    Lesson 169: Initial Commit

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200, //extensão de cada elemento
+        childAspectRatio: 3/2, //proporção de cada elemento
+        crossAxisSpacing: 20, // espaçamento no eixo x
+        mainAxisSpacing: 20, // espaçamento no eixo y
+        
+    ),
+    children: <Widget>[
+      
+    ],
+    );
+  }
+}
